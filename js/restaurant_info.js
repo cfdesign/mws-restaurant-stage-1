3aa918cc46ae33e344421d3dad7bb3722cb25680
Chris Faulds <fauldzy@hotmail.co.uk>
2018-07-20 12:47:34 +0100

    Accessibility changes made

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -127,7 +127,8 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
+  title.tabIndex = "0";
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
